5e6172db004b4c4ba6a08db6268ef38fc542c347
Josh Braden <josbraden@gmail.com>
2021-04-05 09:35:21 -0500

    Basic configuration object

--- nutlogger.h
+++ nutlogger.h
@@ -4,4 +4,17 @@ class argobj {
     bool verbose = false;
 };
 
+//Object for program runtime configurations
+//Read from the config file or copied from args
+class configobj {
+    public:
+    bool verbose = false;
+    int pollinterval = 5;
+    std::string mysql_host = "127.0.0.1";
+	std::string mysql_username = "upsuser";
+	std::string mysql_password = "foobar";
+	std::string mysql_database = "ups";
+	bool mysql_compress = false;
+};
+
 int init(argobj argdata);
