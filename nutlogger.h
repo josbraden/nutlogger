109679c519e2c0ed530387761323005af083e0bb
Josh Braden <josbraden@gmail.com>
2021-04-05 16:35:07 -0500

    Increase default poll interval to 35 - this is closer to the real update time of most UPSs

--- nutlogger.h
+++ nutlogger.h
@@ -11,7 +11,7 @@ class configobj {
     public:
     //Program stuff
     bool verbose = false;
-    int pollinterval = 5;
+    int pollinterval = 35;
     //MySQL stuff
     std::string mysql_host = "127.0.0.1";
 	std::string mysql_username = "upsuser";
