16736022222eb74777208bd24617635f32590b78
Josh Braden <josbraden@gmail.com>
2021-04-06 17:55:38 -0500

    Some structure

--- logger.cpp
+++ logger.cpp
@@ -9,10 +9,29 @@ using namespace std;
 
 int logger(configobj config) {
     //Before starting loop, see if devices are in database yet
+    if (config.verbose) {
+        cout << "Checking database for devices..." << endl;
+    }
     for (long unsigned int i = 0; i < config.upslist.size(); i++) {
         if (getupsknt(config, i) != 1) {
+            if (config.verbose) {
+                cout << "Adding " << config.upslist[i] << " to database" << endl;
+            }
             //Add device to MySQL database
         }
+    }
+    //Get device IDs
+    if (config.verbose) {
+        cout << "Getting device IDs from database" << endl;
+    }
+    //Update 'extra' dvice info
+    if (config.verbose) {
+        cout << "Updating device extra data fields" << endl;
+    }
+    //Other startup stuff?
+    //Start logging loop
+    if (config.verbose) {
+        cout << "Starting logging" << endl;
     }
     return 0;
 }
