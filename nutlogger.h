5e25a0cc3feba624a4134a12f772f41183bf5f39
Josh Braden <josbraden@gmail.com>
2021-04-07 10:12:28 -0500

    Add extra data field object

--- nutlogger.h
+++ nutlogger.h
@@ -31,6 +31,38 @@ class configobj {
     void set_upsidlist();
 };
 
+//Object to hold extra data fields for a UPS device
+class extradataobj {
+    public:
+    int battery_charge_low;
+    int battery_charge_warning;
+    std::string battery_mfr_date;
+    int battery_runtime_low;
+    std::string battery_type;
+    float battery_voltage_nominal;
+    std::string device_mfr;
+    std::string device_model;
+    std::string device_serial;
+    std::string device_type;
+    std::string driver_name;
+    std::string driver_version;
+    std::string driver_version_data;
+    std::string driver_version_internal;
+    float input_voltage_nominal;
+    std::string ups_beeper_status;
+    int ups_delay_shutdown;
+    int ups_delay_start;
+    std::string ups_mfr;
+    std::string ups_model;
+    std::string ups_productid;
+    int ups_realpower_nominal;
+    std::string ups_serial;
+    std::string ups_test_result;
+    int ups_timer_shutdown;
+    int ups_timer_start;
+    int ups_vendorid;
+};
+
 //Init functions
 int init(argobj argdata);
 configobj setconfigfield(configobj config, std::string key, std::string value);
@@ -43,6 +75,7 @@ int testmysql(configobj config);
 int getupsknt(configobj config, int index);
 int addups(configobj config, int index);
 int getupsid(configobj config, int index);
+int updateextradata(configobj config, int index);
 
 //Main loop functions
 int logger(configobj config);
