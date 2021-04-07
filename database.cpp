088fb3b21e2ead7e43b973bbd36bc05f5ea50c8c
Josh Braden <josbraden@gmail.com>
2021-04-07 11:12:57 -0500

    Basic extra data field update query and command

--- database.cpp
+++ database.cpp
@@ -2,6 +2,7 @@
 For database related functionality
 */
 #include <iostream>
+#include <string>
 #include <unistd.h>
 #include "nutlogger.h"
 #include <cppconn/driver.h>
@@ -145,4 +146,95 @@ int getupsid(configobj config, int index) {
 		return -1;
 	}
     return ret;
+}
+
+int updateextradata(configobj config, int index, extradataobj extradata) {
+    //TODO get info from UPSs
+    string query = "INSERT INTO devices_extradata ("
+        "devices_id,"
+        "battery_charge_low,"
+        "battery_charge_warning,"
+        "battery_mfr_date,"
+        "battery_runtime_low,"
+        "battery_type,"
+        "battery_voltage_nominal,"
+        "device_mfr,"
+        "device_model,"
+        "device_serial,"
+        "device_type,"
+        "driver_name,"
+        "driver_version,"
+        "driver_version_data,"
+        "driver_version_internal,"
+        "input_voltage_nominal,"
+        "ups_beeper_status,"
+        "ups_delay_shutdown,"
+        "ups_delay_start,"
+        "ups_mfr,"
+        "ups_model,"
+        "ups_productid,"
+        "ups_realpower_nominal,"
+        "ups_serial,"
+        "ups_test_result,"
+        "ups_timer_shutdown,"
+        "ups_timer_start,"
+        "ups_vendorid"
+        ") VALUES ("
+        + to_string(config.upsidlist[index]) + ","
+        + to_string(extradata.battery_charge_low) + ","
+        + to_string(extradata.battery_charge_warning) + ","
+        + extradata.battery_mfr_date + ","
+        + to_string(extradata.battery_runtime_low) + ","
+        + extradata.battery_type + ","
+        + to_string(extradata.battery_voltage_nominal) + ","
+        + extradata.device_mfr + ","
+        + extradata.device_model + ","
+        + extradata.device_serial + ","
+        + extradata.device_type + ","
+        + extradata.driver_name + ","
+        + extradata.driver_version + ","
+        + extradata.driver_version_data + ","
+        + extradata.driver_version_internal + ","
+        + to_string(extradata.input_voltage_nominal) + ","
+        + extradata.ups_beeper_status + ","
+        + to_string(extradata.ups_delay_shutdown) + ","
+        + to_string(extradata.ups_delay_start) + ","
+        + extradata.ups_mfr + ","
+        + extradata.ups_model + ","
+        + extradata.ups_productid + ","
+        + to_string(extradata.ups_realpower_nominal) + ","
+        + extradata.ups_serial + ","
+        + extradata.ups_test_result + ","
+        + to_string(extradata.ups_timer_shutdown) + ","
+        + to_string(extradata.ups_timer_start) + ","
+        + to_string(extradata.ups_vendorid) + ")"
+        "ON DUPLICATE KEY UPDATE devices_id = devices_id"; //do nothing on duplicate keys
+    try {
+		sql::Driver *driver;
+		sql::Connection *con;
+		sql::ConnectOptionsMap connection_properties;
+		sql::Statement *stmt;
+		sql::ResultSet *res;
+		connection_properties["hostName"] = config.mysql_host;
+		connection_properties["userName"] = config.mysql_username;
+		connection_properties["password"] = config.mysql_password;
+		connection_properties["schema"] = config.mysql_database;
+		connection_properties["CLIENT_COMPRESS"] = config.mysql_compress;
+		driver = get_driver_instance();
+		con = driver->connect(connection_properties);
+		stmt = con->createStatement();
+		res = stmt->executeQuery(query);
+		delete res;
+		delete stmt;
+		delete con;
+	}
+	catch (sql::SQLException &e) {
+		cout << "# ERR: SQLException in " << __FILE__;
+		cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+		cout << "# ERR: " << e.what();
+		cout << " (MySQL error code: " << e.getErrorCode();
+		cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+		return -1;
+	}
+    return 0;
 }
